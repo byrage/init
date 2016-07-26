8969d24b7926b217358a4550e9de8d0fe8bdd550
youngwoong choi <yw.choi@anypointmedia.com>
2016-07-26 14:35:17 +0900

    develop hero customizing scripts containing some feature
    1. preferred application
    2. custom aliases
    3. vim configuration
    4. mint shortcut

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# init
+# this script is to help you while install mint and initilisation is in progress.
+
+You must install ruby and then use this command to run initialisation script.
+
+V0.1 [2016/07/26]
+1. preferred application
+2. custom aliases
+3. vim configuration
+4. mint shortcut
---
+++ mint/script/customize.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+class Customize
+
+  require 'fileutils'
+
+  def self.makeDirectoryIfNotExists(directory)
+    FileUtils.mkdir_p(directory) unless File.exists?(directory)
+    # Same meaning with above line
+    # puts `mkdir -p "#{directory}"`
+  end
+
+  def self.copy(src, dst)
+    makeDirectoryIfNotExists(dst)
+    FileUtils.cp_r(src, dst)
+  end
+
+  def self.catAndAppend(src, dst, fileName)
+    makeDirectoryIfNotExists(dst)
+    puts `cat #{src} >> #{dst+fileName}`
+  end
+
+  puts('input your id : ')
+  # userId = gets.chomp
+  userId = 'hero'
+  homeDirectory = '/home/'+userId+'/test'
+  absolutePath = Dir.pwd
+  myFile =absolutePath.split('/script')[0] + '/myfile/'
+
+  puts('homeDirectory = '+homeDirectory)
+  puts('myFile = '+myFile)
+
+  autoStart = [myFile + 'startup_applications/.', homeDirectory+'/config/autostart']
+  aliases = [myFile + 'alias/*', homeDirectory, '/profile'] # .profile
+  vim = [myFile + '/vim/vimrc', homeDirectory, '/vimrc'] # .vimrc
+  dconf = myFile + '/dconf/dconf-settings.conf'
+
+  # set preferred application
+  copy(autoStart[0], autoStart[1])
+
+  # set custom aliases
+  catAndAppend(aliases[0], aliases[1], aliases[2])
+
+  # set vim configuration
+  catAndAppend(vim[0], vim[1], vim[2])
+
+  # set mint shortcut
+  puts `apt install dconf-cli`
+  puts `dconf load /org/cinnamon/desktop/keybindings/ < "#{dconf}"`
+end
