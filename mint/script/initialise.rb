79af24683af2124c2bf89428ba1bee5672038ef7
youngwoong choi <yw.choi@anypointmedia.com>
2016-07-26 18:07:24 +0900

    remove whitespace that between /etc and filename

--- mint/script/initialise.rb
+++ mint/script/initialise.rb
@@ -79,7 +79,7 @@ class Initialise
 
   # virtual box
   # existCommand vboxmanage
-  puts `echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" > /etc/ apt/sources.list.d/virtualbox.list && \
+  puts `echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" > /etc/apt/sources.list.d/virtualbox.list && \
         wget -q https : // www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add - && \
         apt-get update && \
         apt-get -y install virtualbox-5.0`
@@ -87,7 +87,7 @@ class Initialise
   # chrome
   # existCommand google-chrome
   puts `wget -q -O - https : // dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - && \
-        echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/ apt/sources.list.d/google-chrome.list && \
+        echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list && \
         apt-get update && \
         apt-get -y install google-chrome-stable ttf-unfonts-core`
 
