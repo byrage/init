71658c1cc4805ea43738726d827965d135ed4515
youngwoong choi <yw.choi@anypointmedia.com>
2016-08-03 16:42:43 +0900

    fix customize.rb

--- mint/script/customize.rb
+++ mint/script/customize.rb
@@ -19,6 +19,10 @@ class Customize
     puts `cat #{src} >> #{dst+fileName}`
   end
 
+  def self.tryingMessage(command)
+    puts `echo '\nInstalling #{command} ... \n\n\n'`
+  end
+
   print 'input your id : '
   userId = gets.chomp
   homeDirectory = '/home/'+userId
@@ -30,33 +34,40 @@ class Customize
 
 
   # set preferred application
+  tryingMessage 'set preferred application'
   autoStart = [myFile + 'startup_applications/.', homeDirectory+'/.config/autostart']
   copy autoStart[0], autoStart[1]
   puts `chown -R "#{userId}":"#{userId}" "#{autoStart[1]}"`
 
   # set custom aliases
+  tryingMessage 'set custom aliases'
   aliases = [myFile + 'alias/*', homeDirectory, '/.profile'] # .profile
   catAndAppend aliases[0], aliases[1], aliases[2]
 
   # set vim configuration
+  tryingMessage 'set vim configuration'
   vim = [myFile + '/vim/vimrc', homeDirectory, '/.vimrc'] # .vimrc
   catAndAppend vim[0], vim[1], vim[2]
 
   # set mint shortcut
+  tryingMessage 'set mint shortcut'
   dconf = myFile + '/dconf/dconf-settings.conf'
+  puts `apt-get -f install`
   puts `apt install dconf-cli`
   puts `dconf load /org/cinnamon/desktop/keybindings/ < "#{dconf}"`
 
   # set git alias
-  puts `git config --global alias.co checkout`
-  puts `git config --global alias.br branch -v`
-  puts `git config --global alias.ci commit`
-  puts `git config --global alias.st status`
-  puts `git config --global alias.ammend commit --amend`
-  puts `git config --global alias.unstage reset HEAD --`
-  puts `git config --global alias.last log -1 HEAD`
+  tryingMessage 'set git alias'
+  puts `git config --global alias.co 'checkout'`
+  puts `git config --global alias.br 'branch -v'`
+  puts `git config --global alias.ci 'commit'`
+  puts `git config --global alias.st 'status'`
+  puts `git config --global alias.amend 'commit --amend'`
+  puts `git config --global alias.unstage 'reset HEAD --'`
+  puts `git config --global alias.last 'log -1 HEAD'`
 
   # set terminator configuration
+  tryingMessage 'set terminator configuration'
   terminator = [myFile + '/terminator/config', homeDirectory+'/.config/terminator']
   copy terminator[0], terminator[1]
 
